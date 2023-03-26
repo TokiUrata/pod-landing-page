cf0a6f0e50c1a2f8730eb5d53fdbb32edc97bb44
Toki Urata <tokiurata@outlook.com>
2023-03-25 23:57:54 -0400

    Refactor Modal to implement ReactDOM create portal

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -1,18 +1,24 @@
+import ReactDOM from 'react-dom';
 import Backdrop from './Backdrop';
 import Button from './Button';
 import './Modal.scss';
 
 const Modal = (props) => {
   return (
-    <Backdrop onClick={props.onClick}>
-      <div className={props.className || 'modal'}>
-        <h2 className="modal__title">{props.title}</h2>
-        <p className="modal__subtitle">{props.subtitle}</p>
-        <Button className="button button--modal" onClick={props.onClick}>
-          Close
-        </Button>
-      </div>
-    </Backdrop>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onClick}>
+          <div className={props.className || 'modal'}>
+            <h2 className="modal__title">{props.title}</h2>
+            <p className="modal__subtitle">{props.subtitle}</p>
+            <Button className="button button--modal" onClick={props.onClick}>
+              Close
+            </Button>
+          </div>
+        </Backdrop>,
+        document.getElementById('modal-root')
+      )}
+    </>
   );
 };
 
