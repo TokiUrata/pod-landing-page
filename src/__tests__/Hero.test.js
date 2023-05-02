479b0bb1001cb8a0661121b0e10d9392e6fe22be
Toki Urata <tokiurata@outlook.com>
2023-05-02 18:52:31 -0400

    Add tests to Hero component

---
+++ src/__tests__/Hero.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import Hero from '../components/Hero';
+
+test('Should render the Hero title', () => {
+  render(<Hero />);
+  const titleElement = screen.getByText(/Publish your podcasts/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('Should render the Hero subtitle', () => {
+  render(<Hero />);
+  const subtitleElement = screen.getByText(
+    /Upload your audio to Pod with a single click/i
+  );
+  expect(subtitleElement).toBeInTheDocument();
+});
+
+test('Should render Partners component', () => {
+  render(<Hero />);
+  const partners = screen.getByRole('list');
+  expect(partners).toBeInTheDocument();
+});
+
+test('Should render HeroForm component', () => {
+  render(<Hero />);
+  const heroForm = screen.getByRole('region', {
+    name: /Request access to Pod/i,
+  });
+  expect(heroForm).toBeInTheDocument();
+});
